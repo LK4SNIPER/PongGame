05f46494f52fe56c4086290bed8e95ed52ca3c6e
LK4SNIPER <131406228+LK4SNIPER@users.noreply.github.com>
2023-09-18 23:41:38 -0500

    Beta Pong

--- Pong.py
+++ Pong.py
@@ -1,5 +1,47 @@
 import pygame
 import sys
+import random
+
+# Ball Animation
+def ball_animation():
+    global ball_speed_y, ball_speed_x
+    ball.x += ball_speed_x
+    ball.y += ball_speed_y
+
+    if ball.top <= 0 or ball.bottom >= screen_height:
+        ball_speed_y *= -1
+    if ball.left <= 0 or ball.right >= screen_widght:
+        ball_restart()
+
+    if ball.colliderect(player) or ball.colliderect(opponent):
+        ball_speed_x *= -1
+
+# Player Animation
+def player_animation():
+    player.y += player_speed
+    if player.top <= 0:
+        player.top = 0
+    if player.bottom >= screen_height:
+        player.bottom = screen_height
+
+# Opponent AI
+def opponent_animation():
+    if opponent.top < ball.y:
+        opponent.top += opponent_speed
+    if opponent.bottom > ball.y:
+        opponent.bottom -= opponent_speed
+    if opponent.top <= 0:
+        opponent.top = 0
+    if opponent.bottom >= screen_height:
+        opponent.bottom = screen_height
+
+# Ball restart
+def ball_restart():
+    global ball_speed_x, ball_speed_y
+    ball.center = (screen_widght/2, screen_height/2)
+    ball_speed_y *= random.choice((1, -1))
+    ball_speed_x *= random.choice((1, -1))
+
 
 # General setup
 pygame.init()
@@ -17,12 +59,32 @@ opponent = pygame.Rect(10, screen_height/2 - 70, 10, 140)
 bg_color = pygame.Color("grey12")
 light_grey = (200, 200, 200)
 
+ball_speed_x = 7 * random.choice((1, -1))
+ball_speed_y = 7 * random.choice((1, -1))
+player_speed = 0
+opponent_speed = 7
+
 # Main loop
 while True:
+    # Inputs
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_DOWN:
+                player_speed += 7
+            if event.key == pygame.K_UP:
+                player_speed -= 7
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_DOWN:
+                player_speed -= 7
+            if event.key == pygame.K_UP:
+                player_speed += 7
+
+    ball_animation()
+    player_animation()
+    opponent_animation()
 
     # Visuals
     screen.fill(bg_color)
@@ -32,4 +94,4 @@ while True:
 
     # Updating window
     pygame.display.flip()
-    clock.tick(60)
+    clock.tick(75)
